e02a3eafea250671e11880aa3008ae9bca51ad3e
dani <dan.cozma@russmedia.ro>
2018-06-24 18:35:55 +0300

    same code

--- wp-content/themes/theme1998/index.php
+++ wp-content/themes/theme1998/index.php
@@ -1,14 +1,13 @@
-<?php get_header();
-
- if(have_posts()){
-      while(have_posts()){
-            the_post(); ?>
-
-          <h2><?php  the_title(); ?></h3> 
-           <p><?php the_content(); ?> </p>
-    <?php       
-      }
- }  
-  
+<?php get_header(); 
+		
+    if( have_posts() ):
+      
+      while( have_posts() ): the_post(); ?>
+        
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_content(); ?></p>
+<?php endwhile;
+      
+    endif;
    
  get_footer(); ?>
