ab7c8b1ae37aa06d5add9285b50f13d852c12fd1
dani <dan.cozma@russmedia.ro>
2018-06-28 11:09:16 +0300

    Added some new userrole

--- wp-content/plugins/my_plugin/index.php
+++ wp-content/plugins/my_plugin/index.php
@@ -13,15 +13,31 @@
 
  //Includes
 include('includes/activate.php');
-
-
-
-
  //Hooks
 
 register_activation_hook(__FILE__,'dani_activate_plugin');
 
 
+$result = add_role(
+    'basic_wp_user',
+    __( 'Basic user' ),
+    array(
+        'read'         => true,  // true allows this capability
+        'edit_posts'   => true,
+        'delete_posts' => false, // Use false to explicitly deny
+    )
+);
+if ( null !== $result ) {
+    echo 'Yay! New role created!';
+}
+else {
+    echo 'Oh... the basic_contributor role already exists.';
+}
 
 
+
+function add_roles_on_plugin_activation() {
+    add_role( 'custom_role', 'Basic user', array( 'read' => true, 'edit_posts' => true ) );
+}
+register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
  //Shortcodes
--- wp-content/plugins/test_plugin/index.php
+++ wp-content/plugins/test_plugin/index.php
@@ -9,3 +9,28 @@ function my_activate_plugin(){
 }
 
 register_activation_hook(__FILE__,'my_activate_plugin');
+
+
+$result = add_role(
+    'basic_contributor',
+    __( 'Basic Contributor' ),
+    array(
+        'read'         => true,  // true allows this capability
+        'edit_posts'   => true,
+        'delete_posts' => false, // Use false to explicitly deny
+    )
+);
+if ( null !== $result ) {
+    echo 'Yay! New role created!';
+}
+else {
+    echo 'Oh... the basic_contributor role already exists.';
+}
+
+
+
+function add_roles_on_plugin_activation() {
+    add_role( 'custom_role', 'Basic Contributor', array( 'read' => true, 'edit_posts' => true ) );
+}
+register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
+ //Shortcodes
